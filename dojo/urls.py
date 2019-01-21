7c5cb526c9cabd1a5137bd7ff54bf4bbc29f4a1a
rrabit42 <gegiraffe@naver.com>
2019-01-21 17:04:21 +0900

    dojo.views.mysumm에서 다수 숫자를 받도록 개선

--- dojo/urls.py
+++ dojo/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^sum/(?P<x>\d+)/$', views.mysum),
-    re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
-    re_path(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    #\d나 /가 1번 이상 반복되면 함수 호출
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def mysum(request, x, y=0, z=0):
-    #request: HttpRequest
-    return HttpResponse(int(x) + int(y) + int(z))
+def mysum(request, numbers):
+    # numbers = "1/2/12/123/12312/312/123123"
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    return HttpResponse(result)
 
 
