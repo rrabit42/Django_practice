62dc18b359bce0474124febff0e1ac0bbe65dfb8
rrabit42 <gegiraffe@naver.com>
2019-01-22 03:26:52 +0900

    blog.models.Post 모델 생성/마이그레이션/admin등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,
+                             # choices = (
+                             #     ('제목1', '제목1 레이블'),
+                             #     ('제목2', '제목2 레이블'),
+                             #     ('제목3', '제목3 레이블'),
+                             # )
+                             verbose_name='제목',
+                             help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
