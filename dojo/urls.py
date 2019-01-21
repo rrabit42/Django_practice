d037947ac6179a09127ecebbd6fe1c974a594051
rrabit42 <gegiraffe@naver.com>
2019-01-21 17:08:04 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     #\d나 /가 1번 이상 반복되면 함수 호출
+    re_path('hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/', views.hello),
 ]
