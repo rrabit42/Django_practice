5d9bdd1c796c6598bd64a038cee3659129e4085d
rrabit42 <gegiraffe@naver.com>
2019-01-23 00:26:42 +0900

    blog.models.Post 모델에 대한 PostAdmin 커스텀

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,17 @@ from .models import Post
 
 
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'title', 'created_at', 'updated_at']
+#
+# admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'created_at', 'updated_at']
+
+
