3eebd076f94e4a08aed23d4088cb63ec9eaf90f8
rrabit42 <gegiraffe@naver.com>
2019-01-24 16:04:16 +0900

    blog.models.Comment 모델 생성/admin등록

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list),
+    path('<int:id>/', views.post_detail),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -12,6 +13,18 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
         'q' : q,
+    })
+
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404
+    post = get_object_or_404(Post, id=id)
+
+    return render(request, 'blog/post_detail.html', {
+        'post' : post,
     })
 
 
