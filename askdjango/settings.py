38cf5b25cd28547199b1ec224f18c40cdce6b389
rrabit42 <gegiraffe@naver.com>
2019-01-25 01:17:31 +0900

    template 상속, layout.html 만들기

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'blog',
     'dojo',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,9 @@ ROOT_URLCONF = 'askdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'askdjango', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
