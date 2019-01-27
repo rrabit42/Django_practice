e30791c574603f1ef26aba37b55eea47d9619f6a
rrabit42 <gegiraffe@naver.com>
2019-01-28 00:00:32 +0900

    각 app마다 namespace를 적용

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path
 from . import views
 
+app_name='blog'
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('detail/<int:id>/', views.post_detail, name='post_detail'),
--- dojo/urls.py
+++ dojo/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 from . import views
 from . import views_cbv
 
+app_name="dojo"
+
 urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     #\d나 /가 1번 이상 반복되면 함수 호출
