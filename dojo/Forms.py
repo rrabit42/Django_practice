95cc195dbda22f058c07e04166e267b2f97a1567
rrabit42 <gegiraffe@naver.com>
2019-01-30 02:14:51 +0900

    dojo.models.Post 모델에 validators 적용하고, model_form.save(commit=False) 활용 예시 추가

--- dojo/Forms.py
+++ dojo/Forms.py
@@ -1,14 +1,19 @@
 from django import forms
+from .models import Post
 
 
-def min_length_3_validator(value):
-    if len(value) < 3:
-        raise forms.ValidationError('3글자 이상 입력해주세요.')
 
 
-class PostForm(forms.Form):
-    title = forms.CharField(validators=[min_length_3_validator])
-    content = forms.CharField(widget=forms.Textarea)
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        # fields = '__all__'
+        fields = ['title', 'content']
 
+    # def save(self, commit=True):
+    #     self.instance = Post(**self.cleaned_data)
+    #     if commit:
+    #         self.instance.save()
+    #     return self.instance
 
 
