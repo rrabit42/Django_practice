9ec7214a72c8fc371ad4d20213000658367a17ec
rrabit42 <gegiraffe@naver.com>
2019-02-03 23:35:55 +0900

    CBV : CreateView/UpdateView 구현

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:id>/edit/', views.post_edit, name='post_edit'),
 
     path('cbv/new/', views_cbv.post_new),
+    path('cbv/<int:pk>/edit', views_cbv.post_edit),
 ]
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django import forms
 from .models import Post
 
@@ -7,16 +7,7 @@ post_list = ListView.as_view(model=Post, paginate_by=10)
 
 post_detail = DetailView.as_view(model=Post)
 
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
+post_new = CreateView.as_view(model=Post)
 
-
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-    # success_url = '/...' #원래 어디 url로 이동해준다 지정해줘야하는데 get_absolute_url()있으면 안해도 됨!
-
-post_new = PostCreateView.as_view()
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
 
