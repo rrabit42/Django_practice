2a3c18a4dae88b768f9cb4962d0638bd141f5cab
rrabit42 <gegiraffe@naver.com>
2019-02-06 20:57:08 +0900

    퀴즤를 맞춰야 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,13 @@ from . import views
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
+from .Forms import LoginForm
 
 
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login_form.html', authentication_form = LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGIN_URL), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
